1354ff7c277f87b52ae880f392444fcafb4e2c86
Hassan MANSOURI <mansouri.hassan.ing@gmailcom>
2022-01-09 14:18:58 +0000

    add cpp json project

--- cppJson/cppJson.cpp
+++ cppJson/cppJson.cpp
@@ -1,20 +1,31 @@
-// cppJson.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include "json/json.h"
 #include <iostream>
+#include <memory>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    const std::string rawJson = R"({"Age": 20, "Name": "colin"})";
+    const auto rawJsonLength = static_cast<int>(rawJson.length());
+    constexpr bool shouldUseOldWay = false;
+    JSONCPP_STRING err;
+    Json::Value root;
+
+    if (shouldUseOldWay) {
+        Json::Reader reader;
+        reader.parse(rawJson, root);
+    }
+    else {
+        Json::CharReaderBuilder builder;
+        const std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+        if (!reader->parse(rawJson.c_str(), rawJson.c_str() + rawJsonLength, &root,
+            &err)) {
+            std::cout << "error" << std::endl;
+            return EXIT_FAILURE;
+        }
+    }
+    const std::string name = root["Name"].asString();
+    const int age = root["Age"].asInt();
+
+    std::cout << name << std::endl;
+    std::cout << age << std::endl;
+    return EXIT_SUCCESS;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
